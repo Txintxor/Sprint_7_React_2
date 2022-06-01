4606cc5c91b69c381f654d7025a0fb8a336436ff
AlexQ <78956966+Txintxor@users.noreply.github.com>
2022-06-01 11:51:06 +0200

    COMMIT PER CORREGIR USEFFECTS, exercici 4 completat

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Menu from "./components/menu/Menu.js"
-import { useLocalStorage } from "./components/useLocalStorage";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Menu from "./components/menu/Menu.js";
+import {
+  DataContainer,
+  DivContainer,
+} from "./components/styled-c/styled-components.js";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //ESTOY CON LOCALSTORAGE
 function App() {
@@ -11,10 +14,18 @@ function App() {
   const [extra, setExtra] = useState(0);
   const [page, setPage] = useState(1);
   const [idioma, setIdioma] = useState(1);
-  // const [web, setWeb] = useState("No");
-  // const [seo, setSeo] = useState("No");
-  // const [ads, setAds] = useState("No");
-  // const [data, setData] = useLocalStorage();
+  const [web, setWeb] = useState("No");
+  const [seo, setSeo] = useState("No");
+  const [ads, setAds] = useState("No");
+  const [object, setObject] = useState({
+      web: '', 
+      page: '',
+      idiom: '',
+      seo: '', 
+      ads: '', 
+      preu: ''
+    })
+  const [data, setData] = useState(window.localStorage.getItem("data"));
 
   const changeExtra = (e) => {
     e.target.className.includes("numPage")
@@ -30,15 +41,28 @@ function App() {
       : setIdioma(idioma - 1);
   };
 
-   
+  const setLocalStorage = () => {
+    const pressupost = {
+      web: web,
+      page: page,
+      idioma: idioma,
+      seo: seo,
+      ads: ads,
+      preu: preu,
+    };
 
-  // useEffect(() => {
-  //   setData(window.localStorage.getItem("data"))
-  // }, []);
+    window.localStorage.setItem("data", JSON.stringify(pressupost));
+  };
+  
 
-
-
-  //ESTOS SON LOS USEEFFECTS A LOS QUE ME REFIERO
+  //LOS USEEFFECTS A LOS QUE ME REFERÍA, NO TENGO CLARO QUE LO ESTÉ HACIENDO BIEN
+  
+  useEffect(() => {
+    setData(window.localStorage.getItem("data"));
+    setObject(JSON.parse(data))
+    
+  }, []);
+  
   useEffect(() => {
     const check = document.querySelector("#web");
     if (check.checked) setPreu(preu - extra);
@@ -55,21 +79,31 @@ function App() {
   // RENDER
   return (
     <div className="App">
-      <Menu
-        changeExtra={changeExtra}
-        setPreu={setPreu}
-        setPage={setPage}
-        setIdiom={setIdioma}
-        // setWeb={setWeb}
-        // setSeo={setSeo}
-        // setAds={setAds}
-        // setLocalStorage={setLocalStorage}
-        preu={preu}
-        page={page}
-        idiom={idioma}
-        extra={extra}
-        // data={data}
-      />
+      <DivContainer>
+        <Menu
+          changeExtra={changeExtra}
+          setPreu={setPreu}
+          setPage={setPage}
+          setIdiom={setIdioma}
+          setWeb={setWeb}
+          setSeo={setSeo}
+          setAds={setAds}
+          setLocalStorage={setLocalStorage}
+          preu={preu}
+          page={page}
+          idiom={idioma}
+          extra={extra}
+          data={data}
+        />
+        <DataContainer>
+          <h4 className="dataH">Webs: {object.web}</h4>
+          <h4 className="dataH">Numero de pagines: {object.page}</h4>
+          <h4 className="dataH">Numero d´idiomes: {object.idioma}</h4>
+          <h4 className="dataH">Seo: {object.seo}</h4>
+          <h4 className="dataH">Ads: {object.ads}</h4>
+          <h4 className="dataH">AQUEST ES EL PREU: {object.preu} LURULUS</h4>
+        </DataContainer>
+      </DivContainer>
     </div>
   );
 }
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -17,8 +17,8 @@ const Menu = (props) => {
             value="500"
             onChange={(e) =>
               e.target.checked
-                ? props.setPreu(props.preu + parseInt(e.target.value) + props.extra) && props.setWeb('Si') 
-                : props.setPreu(props.preu - parseInt(e.target.value) - props.extra) && props.setWeb('No')
+                ? (props.setPreu(props.preu + parseInt(e.target.value) + props.extra) , props.setWeb('Si')) 
+                : (props.setPreu(props.preu - parseInt(e.target.value) - props.extra) , props.setWeb('No'))
             }
           />
           <label htmlFor="web">Una pagina web (500&#x20AC;)</label>
@@ -92,8 +92,8 @@ const Menu = (props) => {
             value="300"
             onChange={(e) =>
               e.target.checked
-                ? props.setPreu(props.preu + parseInt(e.target.value)) && props.setSeo('Si')
-                : props.setPreu(props.preu - parseInt(e.target.value)) && props.setSeo('No')
+                ? (props.setPreu(props.preu + parseInt(e.target.value)) , props.setSeo('Si'))
+                : (props.setPreu(props.preu - parseInt(e.target.value)) , props.setSeo('No'))
             }
           />
           <label htmlFor="seo">Una consultoria SEO (300&#x20AC;)</label>
@@ -107,8 +107,8 @@ const Menu = (props) => {
             value="200"
             onChange={(e) =>
               e.target.checked
-                ? props.setPreu(props.preu + parseInt(e.target.value)) && props.setAds('Si')
-                : props.setPreu(props.preu - parseInt(e.target.value)) && props.setAds('No')
+                ? (props.setPreu(props.preu + parseInt(e.target.value)) , props.setAds('Si'))
+                : (props.setPreu(props.preu - parseInt(e.target.value)) , props.setAds('No'))
             }
           />
           <label htmlFor="ads">Una campanya de Google Ads (200&#x20AC;)</label>
@@ -119,26 +119,7 @@ const Menu = (props) => {
       Submit
     </Button>
     </Form>
-    <DataContainer >
-      <h4 className="dataH">
-        Webs: {props.data} 
-      </h4>
-      <h4 className="dataH">
-        Numero de pagines: {props.data} 
-      </h4>
-      <h4 className="dataH">
-        Numero d´idiomes: {props.data} 
-      </h4>
-      <h4 className="dataH">
-        Seo: {props.data} 
-      </h4>
-      <h4 className="dataH">
-        Ads: {props.data} 
-      </h4>
-      <h4 className="dataH">
-        AQUEST ES EL PREU: {props.data} LURULUS
-      </h4>
-    </DataContainer>
+
 </DivContainer>
   );
 };
